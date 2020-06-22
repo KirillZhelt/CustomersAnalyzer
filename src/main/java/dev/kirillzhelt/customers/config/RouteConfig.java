21dd83de032422314de99a34434a1a57e7962ecf
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-06-22 16:56:12 +0300

    Setup all necessary routes

---
+++ src/main/java/dev/kirillzhelt/customers/config/RouteConfig.java
@@ -0,0 +1,40 @@
+package dev.kirillzhelt.customers.config;
+
+import dev.kirillzhelt.customers.handler.ImportHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import static org.springframework.web.reactive.function.server.ServerResponse.status;
+
+@Configuration
+public class RouteConfig {
+
+    private final ImportHandler importHandler;
+
+    public RouteConfig(ImportHandler importHandler) {
+        this.importHandler = importHandler;
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> routes() {
+        return route(GET("/hello-world"), this.importHandler::helloWorld)
+            .and(route(POST("/imports"), this.importHandler::addImport))
+            .and(route(PATCH("/imports/{importId}/citizens/{citizen_id}"),
+                this.importHandler::patchCitizen))
+            .and(route(GET("/imports/{importId}/citizens"),
+                this.importHandler::getImport))
+            .and(route(GET("/imports/{importId}/citizens/birthdays"),
+                this.importHandler::countPresents))
+            .and(route(GET("/imports/{importId}/towns/percentile/age"),
+                this.importHandler::countStatistics));
+    }
+}
---
+++ src/main/java/dev/kirillzhelt/customers/handler/ImportHandler.java
@@ -0,0 +1,42 @@
+package dev.kirillzhelt.customers.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import static org.springframework.web.reactive.function.server.ServerResponse.status;
+
+@Component
+public class ImportHandler {
+
+    public Mono<ServerResponse> helloWorld(ServerRequest req) {
+        return ok().bodyValue("Hello World");
+    }
+
+    public Mono<ServerResponse> addImport(ServerRequest req) {
+        return status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    public Mono<ServerResponse> patchCitizen(ServerRequest req) {
+        return status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    public Mono<ServerResponse> getImport(ServerRequest req) {
+        return status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    public Mono<ServerResponse> countPresents(ServerRequest req) {
+        return status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    public Mono<ServerResponse> countStatistics(ServerRequest req) {
+        return status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+}
