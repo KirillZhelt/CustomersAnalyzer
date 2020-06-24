a5e27e25f7017602c60775ac0aedf9fe1f3d98fd
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-06-24 15:15:48 +0300

    Implement getting import data

--- src/main/java/dev/kirillzhelt/customers/repository/RelativeRepository.java
+++ src/main/java/dev/kirillzhelt/customers/repository/RelativeRepository.java
@@ -1,9 +1,14 @@
 package dev.kirillzhelt.customers.repository;
 
 import dev.kirillzhelt.customers.entity.Relative;
+import org.reactivestreams.Publisher;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface RelativeRepository extends ReactiveCrudRepository<Relative, Integer> {
+
+    Flux<Relative> findAllByCitizenIdAndImportId(Integer citizenId, Integer importId);
+
 }
