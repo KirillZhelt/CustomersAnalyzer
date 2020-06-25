513e541d65fb4a52fcca2acb282755795e6e3050
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-06-25 18:29:37 +0300

    Implement updating citizen's information

--- src/main/java/dev/kirillzhelt/customers/dto/PatchCitizenDTO.java
+++ src/main/java/dev/kirillzhelt/customers/dto/PatchCitizenDTO.java
@@ -3,17 +3,31 @@ package dev.kirillzhelt.customers.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.kirillzhelt.customers.entity.util.Gender;
+import dev.kirillzhelt.customers.validator.constraint.OneNumberOrLetterConstraint;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.List;
 
 public class PatchCitizenDTO {
 
+    @Size(min = 1, max = 256)
+    @OneNumberOrLetterConstraint
     private String town;
+
+    @Size(min = 1, max = 256)
+    @OneNumberOrLetterConstraint
     private String street;
+
+    @Size(min = 1, max = 256)
+    @OneNumberOrLetterConstraint
     private String building;
+
+    @Min(0)
     private Integer apartment;
 
+    @Size(min = 1, max = 256)
     private String name;
 
     @JsonProperty("birth_date")
@@ -24,5 +38,84 @@ public class PatchCitizenDTO {
 
     private List<Integer> relatives;
 
+    public PatchCitizenDTO() {
+    }
 
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public Integer getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(Integer apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public List<Integer> getRelatives() {
+        return relatives;
+    }
+
+    public void setRelatives(List<Integer> relatives) {
+        this.relatives = relatives;
+    }
+
+    @Override
+    public String toString() {
+        return "PatchCitizenDTO{" +
+            "town='" + town + '\'' +
+            ", street='" + street + '\'' +
+            ", building='" + building + '\'' +
+            ", apartment=" + apartment +
+            ", name='" + name + '\'' +
+            ", birthDate=" + birthDate +
+            ", gender=" + gender +
+            ", relatives=" + relatives +
+            '}';
+    }
 }
--- src/main/java/dev/kirillzhelt/customers/repository/CitizenRepository.java
+++ src/main/java/dev/kirillzhelt/customers/repository/CitizenRepository.java
@@ -4,10 +4,12 @@ import dev.kirillzhelt.customers.entity.Citizen;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface CitizenRepository extends ReactiveCrudRepository<Citizen, Integer> {
 
     Flux<Citizen> findAllByImportId(Integer importId);
+    Mono<Citizen> findByImportIdAndCitizenId(Integer importId, Integer citizenId);
 
 }
