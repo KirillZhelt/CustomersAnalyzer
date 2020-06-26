084e67498675dd08007d6d321349cc490ad859d1
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-06-26 14:33:17 +0300

    Implement counting percentiles

--- src/main/java/dev/kirillzhelt/customers/repository/CitizenRepository.java
+++ src/main/java/dev/kirillzhelt/customers/repository/CitizenRepository.java
@@ -1,6 +1,8 @@
 package dev.kirillzhelt.customers.repository;
 
 import dev.kirillzhelt.customers.entity.Citizen;
+import dev.kirillzhelt.customers.entity.projection.AgesForCityInfo;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -11,5 +13,14 @@ public interface CitizenRepository extends ReactiveCrudRepository<Citizen, Integ
 
     Flux<Citizen> findAllByImportId(Integer importId);
     Mono<Citizen> findByImportIdAndCitizenId(Integer importId, Integer citizenId);
+
+    @Query("SELECT c.town as town, " +
+        "percentile_cont(0.5) WITHIN GROUP (ORDER BY EXTRACT (YEAR FROM age(timezone('utc', now()), c.birth_date))) as p50, " +
+        "percentile_cont(0.75) WITHIN GROUP (ORDER BY EXTRACT (YEAR FROM age(timezone('utc', now()), c.birth_date))) as p75, " +
+        "percentile_cont(0.99) WITHIN GROUP (ORDER BY EXTRACT (YEAR FROM age(timezone('utc', now()), c.birth_date))) as p99 " +
+        "FROM citizen c " +
+        "WHERE c.import_id = :importId " +
+        "GROUP BY town")
+    Flux<AgesForCityInfo> countPercentiles(Integer importId);
 
 }
